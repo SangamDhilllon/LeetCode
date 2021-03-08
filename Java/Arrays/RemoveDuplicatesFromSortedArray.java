69d935d2e63faab6198b97808f564d3e80f8e10c
Sangam Dhillon <51434157+SangamDhilllon@users.noreply.github.com>
2021-03-08 10:53:23 -0500

    Update
    new strategy

--- Java/Arrays/RemoveDuplicatesFromSortedArray.java
+++ Java/Arrays/RemoveDuplicatesFromSortedArray.java
@@ -1,24 +1,21 @@
 package com.demo;
 
 public class RemoveDuplicatesFromSortedArray {
-	//Given a sorted array nums, remove the duplicates in-place 
-	//each element appears only once and returns the new length.
+	// Given a sorted array nums, remove the duplicates in-place
+	// each element appears only once and returns the new length.
 	public static void main(String[] args) {
-		int[] arr= {1,2,2,3,4};
-		int len=removeDuplicates(arr);
-		for(int i=0;i<len;i++) {
+		int[] arr = { 1, 2, 2, 3, 4 };
+		int len = removeDuplicates(arr);
+		for (int i = 0; i < len; i++) {
 			System.out.println(arr[i]);
 		}
 	}
-	
-	public static int removeDuplicates(int[] nums) {        
-        int i=0; //first pointer
-        for (int j=1;j< nums.length;j++) { //second pointer
-			if(nums[j]!=nums[i]) {
-				i++;
-				nums[i]=nums[j];
-			}
-		}
-        return i+1;
-    }
+
+	public static int removeDuplicates(int[] nums) {
+		int i = 0;
+		for (int n : nums)
+			if (i == 0 || n > nums[i - 1])
+				nums[i++] = n;
+		return i;
+	}
 }
